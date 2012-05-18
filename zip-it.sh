71bb1d5713a5a83353fb5ee8c509ce9bba4f2bb7
Michal Migurski <migurski@brillo.(none)>
2012-05-17 23:36:52 -0700

    Cleaned up and commented shellscript

--- zip-it.sh
+++ zip-it.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-SOURCE=$1
-ZIPFILE=$2
-BASE=${ZIPFILE%.zip}
+shapefile=$1
+zipfile=$2
+base=${zipfile%.zip}
 
 # Spherical mercator extent and projection,
 # http://proj.maptools.org/faq.html#sphere_as_wgs84
-
+#
 EXTENT="-180 -85.05112878 180 85.05112878"
 P900913="+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs"
 
-ogr2ogr --config OGR_ENABLE_PARTIAL_REPROJECTION TRUE -t_srs "$P900913" -clipsrc $EXTENT -segmentize 1 -skipfailures $BASE.shp $SOURCE
+# Use http://trac.osgeo.org/gdal/wiki/ConfigOptions#OGR_ENABLE_PARTIAL_REPROJECTION
+# and clip source to include only data within spherical mercator world square.
+#
+ogr2ogr --config OGR_ENABLE_PARTIAL_REPROJECTION TRUE -t_srs "$P900913" -clipsrc $EXTENT -segmentize 1 -skipfailures $base.shp $shapefile
 
-ogrinfo -so $BASE.shp $BASE | tail -n +4 > info.txt
-shapeindex $BASE.shp
-zip -j $ZIPFILE $BASE.dbf $BASE.index $BASE.prj $BASE.shp $BASE.shx info.txt
-rm -f $BASE.dbf $BASE.index $BASE.prj $BASE.shp $BASE.shx info.txt
+# Index the shapefile for Mapnik
+# https://github.com/mapnik/mapnik/tree/master/utils/shapeindex
+#
+shapeindex $base.shp
+
+ogrinfo -so $base.shp $base | tail -n +4 > info.txt
+zip -j $zipfile $base.dbf $base.index $base.prj $base.shp $base.shx info.txt
+rm -f $base.dbf $base.index $base.prj $base.shp $base.shx info.txt
