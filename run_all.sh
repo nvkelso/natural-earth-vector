23244fd310b890b0ccd150fae6f0a03589f38735
ImreSamu <ImreSamu@users.noreply.github.com>
2018-05-19 20:36:55 +0200

    add docker version

---
+++ run_all.sh
@@ -0,0 +1,9 @@
+
+
+python3 ./tools/wikidata/platform_debug_info.py
+time ./tools/wikidata/update.sh fetch
+time ./tools/wikidata/update.sh write
+cat   x_tempshape/update.md
+cp -r x_tempshape/10m_cultural/*    10m_cultural/
+cp -r x_tempshape/10m_physical/*    10m_physical/
+make clean all
