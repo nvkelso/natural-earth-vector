6c1a0d1216ffd4788eab2246a823bce0db1afcab
Stephanie May <mizmay@gmail.com>
2020-12-29 12:38:30 -0800

    Add output file

--- updates/Fix_10m_Populated_Places_WikidataIDs.R
+++ updates/Fix_10m_Populated_Places_WikidataIDs.R
@@ -1,7 +1,6 @@
 library(sf)
 library(WikidataR)
 library(tidyverse)
-library(compare)
 
 setwd("~/Repos/natural-earth-vector/10m_cultural/")
 
@@ -28,3 +27,12 @@ ne_10m_populated_places_modified_metadata <- ne_10m_populated_places %>%
   rows_update(by = "ne_id", tibble(ne_id = 1159149137,
                                    wikidataid = 'Q75379'))
 
+diff <- anti_join(ne_10m_populated_places_original_metadata, 
+                  ne_10m_populated_places_modified_metadata)
+
+write.csv(diff, "Fix_10m_Populated_Places_WikidataIDs.csv", na = "")
+
+# Requires a bit more tweaking to ensure DBF encoding and data types aren't messed up in the transformation.
+#ne_10m_populated_places_modified <- st_set_geometry(ne_10m_populated_places_modified_metadata, 
+#                                                    ne_10m_populated_places$geometry)
+#st_write(ne_10m_populated_places_modified, 'ne_10m_populated_places_wikidataid_update.shp', overwrite = TRUE)
